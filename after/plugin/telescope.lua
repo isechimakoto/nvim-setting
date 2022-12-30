0ecb8436e5480eb896e9d2a34e76580d2244826f
Makoto Isechi <isechimakoto@gmail.com>
2022-12-30 14:43:30 +0900

    Change the telescope keymapping

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,11 +1,27 @@
-require('telescope').load_extension('projects')
+local telescope = require('telescope')
+local actions = require('telescope.actions') 
+telescope.load_extension('projects')
+telescope.setup{
+    defaults = {
+        mappings = {
+            i = {
+                ["<C-j>"] = actions.move_selection_next,
+                ["<C-k>"] = actions.move_selection_previous,
+                ["<C-n>"] = actions.cycle_history_next,
+                ["<C-p>"] = actions.cycle_history_prev,
+            },
+            n = {
+                ["<C-j>"] = actions.move_selection_next,
+                ["<C-k>"] = actions.move_selection_previous,
+            },
+        }
+    }
+}
 local builtin = require('telescope.builtin')
 
 vim.keymap.set('n', '<leader>f', builtin.find_files, {})
 vim.keymap.set('n', '<leader>sr', builtin.oldfiles, {})
-vim.keymap.set('n', '<leader>ps', function()
-    builtin.grep_string({ search = vim.fn.input("Grep > ") })
-end)
+vim.keymap.set('n', '<leader>st', builtin.live_grep) 
 vim.keymap.set('n', '<leader>ls', builtin.lsp_document_symbols, {})
 vim.keymap.set('n', '<leader>lS', builtin.diagnostics, {})
 vim.keymap.set('n', '<leader>P', function()

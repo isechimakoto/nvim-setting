2a0f672242f5b5ee91546c7a81133c0ffa552aad
Makoto Isechi <isechimakoto@gmail.com>
2022-12-31 01:21:38 +0900

    toggle terminal keymap setting

--- lua/misechi/remap.lua
+++ lua/misechi/remap.lua
@@ -34,3 +34,4 @@ vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
 vim.keymap.set("n", "<leader>S", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>")
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+vim.keymap.set("n", "<leader>h", "<cmd>nohlsearch<CR>")

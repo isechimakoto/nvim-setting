df505679b380c985adb27f75848b1ff02456bf9f
Makoto Isechi <isechimakoto@gmail.com>
2023-01-01 21:36:19 +0900

    Change the trigger to toggle normal or insert mode on floating terminal

--- after/plugin/toggleterm.lua
+++ after/plugin/toggleterm.lua
@@ -9,7 +9,7 @@ vim.api.nvim_set_keymap("n", "<leader>gg", "<cmd>lua _lazygit_toggle()<CR>", { n
 
 function _set_terminal_keymaps()
   local opts = {buffer = 0}
-  vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+  vim.keymap.set('t', '<C-q>', [[<C-\><C-n>]], opts)
   vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
   vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
   vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)

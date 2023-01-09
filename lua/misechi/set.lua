041e3990421b889235db3c015300791d522bd055
Makoto Isechi <isechimakoto@gmail.com>
2023-01-09 23:06:09 +0900

    fix clipboard setting

--- lua/misechi/set.lua
+++ lua/misechi/set.lua
@@ -29,8 +29,7 @@ local is_win = has "win32"
 
 if is_mac then
   vim.opt.clipboard:append { 'unnamedplus' }
-end
-if is_win then
+elseif is_win then
   vim.opt.clipboard:prepend { 'unnamed', 'unnamedplus'}
   vim.g.clipboard = {
     name = "xsel_override",

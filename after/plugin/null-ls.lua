fff6f6cdd2bcf66f2976cf8c0f164216b932c71c
Makoto Isechi <isechimakoto@gmail.com>
2023-05-14 22:14:40 +0900

    enabling eslint_d

--- after/plugin/null-ls.lua
+++ after/plugin/null-ls.lua
@@ -8,6 +8,9 @@ null_ls.setup({
   sources = {
     null_ls.builtins.formatting.prettierd,
     null_ls.builtins.diagnostics.eslint_d,
+    null_ls.builtins.code_actions.eslint_d,
+    -- null_ls.builtins.diagnostics.eslint,
+    -- null_ls.builtins.code_actions.eslint,
     null_ls.builtins.diagnostics.phpstan,
     null_ls.builtins.formatting.phpcsfixer,
   },
